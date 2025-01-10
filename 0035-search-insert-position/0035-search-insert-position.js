98a5ffabafab28a0c2505763aba660499dc3c3ce
Jihyun Jeon <80092348+jihyun-j@users.noreply.github.com>
2025-01-10 22:17:58 +0900

    Time: 0 ms (100%), Space: 49.4 MB (23.83%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -3,14 +3,23 @@
  * @param {number} target
  * @return {number}
  */
+
 var searchInsert = function(nums, target) {
-    if(nums.indexOf(target) === -1) {
-        nums.push(target)
-        nums.sort((a,b) => a-b)
-        return nums.indexOf(target)
-    } else {
-        return nums.indexOf(target)
+   let left = 0 
+   let right = nums.length - 1
+
+    while(left <= right) {
+        let mid = Math.floor((left + right) / 2)
+
+        if(nums[mid] === target) {
+            return mid
+        } else if(nums[mid] < target) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
     }
 
+    return left
     
 };
