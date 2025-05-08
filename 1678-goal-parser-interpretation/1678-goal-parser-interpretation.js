98829a32ea9adae673deee7f195bc996ec52583d
Jihyun Jeon <80092348+jihyun-j@users.noreply.github.com>
2025-05-09 07:10:18 +0900

    Time: 45 ms (35.02%), Space: 53.6 MB (58.59%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
@@ -3,16 +3,5 @@
  * @return {string}
  */
 var interpret = function(command) {
-    let result = []
-
-    for(let i=0; i<=command.length; i++){
-        if(command[i] === '(' && command[i+1] === ')') {
-            result.push("o")
-        } else if(command[i] === '(' || command[i] === ')') {
-            result.filter((str) => str !==  "(" || str !== ")" )
-        }else {
-            result.push(command[i])
-        }
-    }
-    return result.join('')
+   return command.split("()").join("o").split("(al)").join("al")
 };
