90e8dacc8282f14f22f0dce8dab01c8785e18770
Jihyun Jeon <80092348+jihyun-j@users.noreply.github.com>
2025-05-10 11:26:52 +0900

    Time: 0 ms (100%), Space: 56.7 MB (7.5%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
@@ -9,7 +9,6 @@ var differenceOfSums = function(n, m) {
 
     for(let i=1; i<=n; i++){
         if(i % m !== 0) {
-            console.log(i)
             num1 = num1 + i
         } else {
             num2 = num2 + i
