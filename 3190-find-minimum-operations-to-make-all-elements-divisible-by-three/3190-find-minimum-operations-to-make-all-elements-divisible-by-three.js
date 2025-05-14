86c3ff7ab45c0f7a05f85352b9043305a6dfe15d
Jihyun Jeon <80092348+jihyun-j@users.noreply.github.com>
2025-05-14 21:10:48 +0900

    Time: 0 ms (100%), Space: 55.6 MB (22.02%) - LeetHub

--- 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.js
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.js
@@ -3,13 +3,5 @@
  * @return {number}
  */
 var minimumOperations = function(nums) {
-    let answer = 0
-
-    for(let i=0; i<nums.length; i++){
-        if(nums[i] % 3 !== 0){
-            answer = answer + 1
-        } 
-    }
-
-    return answer
+    return nums.filter((num) => num % 3 !== 0).length
 };
