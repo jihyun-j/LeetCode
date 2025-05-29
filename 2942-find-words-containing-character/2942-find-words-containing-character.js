93b4a43df03bf7bc4e99ec003dc148e8345624e4
Jihyun Jeon <80092348+jihyun-j@users.noreply.github.com>
2025-05-29 19:52:52 +0900

    Time: 1 ms (82.36%), Space: 59 MB (16.49%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.js
+++ 2942-find-words-containing-character/2942-find-words-containing-character.js
@@ -4,13 +4,5 @@
  * @return {number[]}
  */
 var findWordsContaining = function(words, x) {
-    let indices = []
-
-    for(let i=0; i<words.length; i++){
-        if(words[i].includes(x)){
-            indices.push(i)
-        }
-    }
-
-    return indices
+  return words.reduce((res, word, idx) => word.includes(x) ? [...res, idx] : res, [])
 };
