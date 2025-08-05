fcc12068f9a9d3344d6df5173b6b8f474fc5b3e8
Jihyun Jeon <80092348+jihyun-j@users.noreply.github.com>
2025-08-05 07:33:45 -0700

    Time: 0 ms (100%), Space: 57.2 MB (5.32%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.js
+++ 3110-score-of-a-string/3110-score-of-a-string.js
@@ -4,11 +4,11 @@
  */
 var scoreOfString = function(s) {
     let sum = 0
-    for(let i=0; i < s.length-1; i++){
-        let a = +s.charCodeAt(i) 
-        let b = +s.charCodeAt(i+1)
-        sum += Math.abs(a-b)
+    for(let i = 0; i < s.length -1; i++) {
+        let a = s.charCodeAt(i)
+        let b = s.charCodeAt(i+1)
+        sum = Math.abs(a-b) + sum
     }
-
+    
     return sum
 };
