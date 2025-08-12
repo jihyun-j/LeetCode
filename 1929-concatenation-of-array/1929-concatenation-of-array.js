7e6b32d724666c0f45d178b44e6bc51ad7e3af87
Jihyun Jeon <80092348+jihyun-j@users.noreply.github.com>
2025-08-12 06:23:58 -0700

    Time: 1 ms (61.37%), Space: 58.7 MB (44.91%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,5 +3,13 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    return [...nums, ...nums]
+    const n = nums.length
+    const ans = []
+
+    for(let i=0; i<n; i++){
+        ans[i] = nums[i]
+        ans[i + n] = nums[i]
+    }
+
+    return ans
 };
