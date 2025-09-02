d05bc33bde153235e2602f458e0a8df509a8f0e7
Jihyun Jeon <80092348+jihyun-j@users.noreply.github.com>
2025-09-02 12:50:02 +0900

    Time: 37 ms (77.28%), Space: 53 MB (79.89%) - LeetHub

--- 2620-counter/2620-counter.js
+++ 2620-counter/2620-counter.js
@@ -2,9 +2,10 @@
  * @param {number} n
  * @return {Function} counter
  */
-var createCounter = function(n) {
-    return function() {
-       return n++
+var createCounter = function (n) {
+
+    return function () {
+        return n++
     };
 };
 
