1d30447e16c48ad551708c8783c163bbd015f9a0
Jihyun Jeon <80092348+jihyun-j@users.noreply.github.com>
2025-09-10 22:34:08 +0900

    Time: 58 ms (31.45%), Space: 55.9 MB (10.63%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -4,14 +4,13 @@
  */
 var compose = function(functions) {
     return function(x) {
-        let result = x
-            for(let i=functions.length - 1 ; i >= 0 ; i--) {
-              result = functions[i](result)
+        let res = x
+        for(let i=functions.length-1; i>=0; i--){
+            res = functions[i](res)
         }
-        return result
+    return res
     }
-}
-
+};
 
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
